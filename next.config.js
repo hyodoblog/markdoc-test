6298a2f698e6c32745caf0abfa4de1c04387f45d
hyodoblog <yusei.hyodo@gmail.com>
2022-06-07 12:55:55 +0900

    markdocのテスト完了

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withMarkdoc = require("@markdoc/next.js");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ["md", "mdoc", "ts", "tsx"],
-};
-
-module.exports = withMarkdoc(nextConfig);
+module.exports = withMarkdoc(/* options */)({
+  pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
+});
